515e7f3cfcb4b77cbec7741528b2a8486d91970e
rwparrish <rino3717@gmail.com>
2020-10-23 12:09:23 -0600

    change state of textLength

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import './App.css';
 import { Component } from 'react';
 
-class App extends Component() {
+class App extends Component {
+
+  state = {
+    text: " ",
+    textLength: null
+  };
+
+  textLengthHandler = (event) => {
+    let textLength = {
+      ...this.state.textLength
+    }
+
+    textLength = event.target.value.length
+
+    this.setState({
+      textLength: textLength
+    })
+  };
+
   render() {
     return (
       <div className="App">
+        <input type="text" onChange={this.textLengthHandler} />
+        <p>{this.state.textLength}</p>   
       </div>
     );
   }
-}
+};
 
 export default App;
